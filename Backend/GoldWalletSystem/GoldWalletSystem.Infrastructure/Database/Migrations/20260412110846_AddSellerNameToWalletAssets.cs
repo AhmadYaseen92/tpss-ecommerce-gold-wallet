7688c6e2f4078683f63b107bd1e2884998e3a2c6
AhmadYaseen92 <ahmadyaseen092@gmail.com>
2026-04-12 14:50:17 +0300

    ..

---
+++ Backend/GoldWalletSystem/GoldWalletSystem.Infrastructure/Database/Migrations/20260412110846_AddSellerNameToWalletAssets.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoldWalletSystem.Infrastructure.Database.Migrations
+{
+    public partial class AddSellerNameToWalletAssets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+IF COL_LENGTH('WalletAssets', 'SellerName') IS NULL
+BEGIN
+    ALTER TABLE [WalletAssets]
+    ADD [SellerName] nvarchar(200) NOT NULL
+        CONSTRAINT [DF_WalletAssets_SellerName] DEFAULT N'';
+END
+""");
+
+            migrationBuilder.Sql("""
+UPDATE [WalletAssets]
+SET [SellerName] = CASE
+    WHEN [AssetType] = 5 THEN N'Bullion House'
+    WHEN [AssetType] = 6 THEN N'Gold Palace'
+    ELSE N'Imseeh'
+END
+WHERE [SellerName] = N'';
+""");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+IF COL_LENGTH('WalletAssets', 'SellerName') IS NOT NULL
+BEGIN
+    ALTER TABLE [WalletAssets] DROP CONSTRAINT [DF_WalletAssets_SellerName];
+    ALTER TABLE [WalletAssets] DROP COLUMN [SellerName];
+END
+""");
+        }
+    }
+}
