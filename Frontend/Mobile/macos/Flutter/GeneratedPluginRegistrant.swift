f64e38bcbae9085974aeae10fdb4f8ea4e928d23
mac local changes <>
2026-04-19 18:57:49 +0300

    fix dependencies issue

--- Frontend/Mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Frontend/Mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import file_saver
 import local_auth_darwin
+import path_provider_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
